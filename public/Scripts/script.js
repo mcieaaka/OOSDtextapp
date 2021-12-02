d72086d7265d8c431e23c952d56723cce4d92516
Harshit Srivastava <58990472+mcieaaka@users.noreply.github.com>
2021-12-02 16:24:00 +0530

    Update script.js

--- public/Scripts/script.js
+++ public/Scripts/script.js
@@ -2,7 +2,7 @@ window.onload = function() {
     window.scrollTo(0,document.body.scrollHeight);
 }
 
-var socket = io("http://ec2-13-235-246-55.ap-south-1.compute.amazonaws.com:4000");
+var socket = io("http://ec2-13-235-243-142.ap-south-1.compute.amazonaws.com:4000");
 socket.on('connect',()=>{
     console.log(socket.id);
 })
