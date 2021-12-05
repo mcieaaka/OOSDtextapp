911f1c166ebc89cc6d4c90e18350d62226404d59
Harshit <harshit0300@gmail.com>
2021-12-05 17:18:06 +0530

    mobile web view

--- android/ez_chat/lib/main.dart
+++ android/ez_chat/lib/main.dart
@@ -8,7 +8,7 @@ void main() {
       home: Scaffold(
         body: WebView(
           initialUrl:
-              "http://ec2-13-235-246-55.ap-south-1.compute.amazonaws.com:3000/", //My dad's website
+              "http://ec2-13-235-243-142.ap-south-1.compute.amazonaws.com:4000/", //My dad's website
           javascriptMode: JavascriptMode.unrestricted,
         ),
       ),
